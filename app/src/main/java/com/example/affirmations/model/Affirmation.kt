ce6e71748a4097dac1373d7e59a568097db44b0a
Khang-Truong <khangtruong1995@gmail.com>
2022-05-02 21:38:00 -0700

    Add new project

---
+++ app/src/main/java/com/example/affirmations/model/Affirmation.kt
@@ -0,0 +1,6 @@
+package com.example.affirmations.model
+
+//Make Affirmation a data class by adding the data keyword.
+//data classes must have at least one property defined.
+data class Affirmation (val stringResourceId: Int, val imageResourceId: Int) {
+}
